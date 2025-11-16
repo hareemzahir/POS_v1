3413483858c2542fb5a3bdd876cf2ddfc3b57959
Hareem Zahir <hareemzahir@gmail.com>
2025-11-16 19:52:28 +0500

    Initial commit

---
+++ src/com/pos/controller/DashboardController.java
@@ -0,0 +1,11 @@
+package com.pos.controller;
+
+import javafx.fxml.FXML;
+
+public class DashboardController {
+
+    @FXML
+    public void initialize() {
+        // For now, this is empty. You can later handle tab events if needed.
+    }
+}
